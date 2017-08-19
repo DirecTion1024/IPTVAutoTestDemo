d7947accfe3f29fe40eed62b645eb69de738bc9a
Administrator <Administrator@WIN10-706082301>
2017-08-19 16:28:44 +0800

    add keycode

--- NewLibrary/myclass.py
+++ NewLibrary/myclass.py
@@ -10,11 +10,19 @@ class MyClass(object):
     
     def rc_down(self):
         os.popen('adb shell input keyevent 20')
+    
+    def rc_up(self):
+        os.popen('adb shell input keyevent 19')
+    
+    def rc_left(self):
+        os.popen('adb shell input keyevent 21')
+    
+    def rc_right(self):
+        os.popen('adb shell input keyevent 22')
+    
 		
     def searchContent(self,content):
         search = 'adb shell am startservice -a com.iflytek.xiri2.START --es startmode text --es text '+content
         print search
         os.popen('adb shell am startservice -a com.iflytek.xiri2.START --es startmode text --es text ���ǳ۵ĵ�Ӱ')    
     
-    def rc_right(self):
-        os.popen('adb shell input keyevent 20')
