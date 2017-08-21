12d21bf6d17134f049faec8d616648b5c7e837c1
DirecTion1024 <fx18358196136@gmail.com>
2017-08-21 20:14:03 +0800

    add readfile

--- NewLibrary/myclass.py
+++ NewLibrary/myclass.py
@@ -28,7 +28,8 @@ class MyClass(object):
     def searchContent(self,content):
         search = 'adb shell am startservice -a com.iflytek.xiri2.START --es startmode text --es text '+content
         print search
-        os.popen(search)   
+        fhandle = open('f:\search.txt')
+        os.system(fhandle.read())   
     
     def make_odd(self,startnum,endnum):
         while startnum <= endnum:
@@ -44,6 +45,9 @@ class MyClass(object):
             startnum + 1
         return list
     
+    def read_file(self,filelocation):
+        fhandle = open(filelocation)
+        print fhandle.read()
     
     
      
