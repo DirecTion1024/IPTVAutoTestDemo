10b393c6be0412e1b6ea69b78877f6d4d86451bf
DirecTion1024 <fx18358196136@gmail.com>
2017-09-21 19:19:18 +0800

    update selenium2

--- selenium2/wdriver.py
+++ selenium2/wdriver.py
@@ -2,14 +2,58 @@
 import os
 from selenium2 import webdriver 
 from time import sleep
+import linecache
+import re
+def open_url(url):
+    content = "adb shell am start -n com.fiberhome.iptv/.FHIptv --es intentMsg " + "\"" + str(url) + "\""
+    print content
+    os.system(content)
+        
+def youku_search(filepath,linenum):
+    print filepath
+    print linenum
+    content = linecache.getline(str(filepath),int(linenum))
+    #command = content.decode("utf-8").encode("gbk")
+    print content
+    return content   
+        
+def read_id(content):
+    m = re.search(r'[^=]+$', content)
+    if m:
+        id = m.group(0)
+        return id[0:-1]
+    else:
+        return 'not search'
+        
+def wirte_youku_file(urlid,content):
+    filelocation = "F:\\youku\\" +urlid +".txt"
+    print filelocation
+    fhandle = open(str(filelocation),'w')
+    source = content.encode("utf-8")
+    fhandle.write(source)
+    fhandle.close()
+        
+def open_youku():
+    command = 'adb shell am startservice -a com.iflytek.xiri2.START --es startmode text --es text 打开优酷'
+    print command.decode("utf-8").encode("gbk")
+    os.popen(command.decode("utf-8").encode("gbk"))
 
-command = 'adb shell am startservice -a com.iflytek.xiri2.START --es startmode text --es text 打开优酷'
-print command.decode("utf-8").encode("gbk")
-os.popen(command.decode("utf-8").encode("gbk"))
+def open_chrome_driver():
+    os.popen("chromedriver.exe --url-base=wd/hub --port=8000 --adb-port=5037")
+   
+
+open_chrome_driver()
+sleep(1)
+open_youku()
 sleep(2)
-content = "adb shell am start -n com.fiberhome.iptv/.FHIptv --es intentMsg " + "\"" + "http://itvmkt.ah163.net/youku/detail.html?video_id=3943" + "\""
-print content
-os.popen(content)
+open_url(youku_search("F:\youku.txt",1))
+#command = 'adb shell am startservice -a com.iflytek.xiri2.START --es startmode text --es text 打开优酷'
+#print command.decode("utf-8").encode("gbk")
+#os.popen(command.decode("utf-8").encode("gbk"))
+
+#content = "adb shell am start -n com.fiberhome.iptv/.FHIptv --es intentMsg " + "\"" + "http://itvmkt.ah163.net/youku/detail.html?video_id=3943" + "\""
+#print content
+#os.popen(content)
 sleep(2)
 driver = webdriver.Remote("http://127.0.0.1:8000/wd/hub", {"desiredCapabilities":
                                                           {"chromeOptions":
@@ -23,4 +67,10 @@ driver = webdriver.Remote("http://127.0.0.1:8000/wd/hub", {"desiredCapabilities"
                           )
 
 driver.switch_to_frame("mainWin")
-print driver.page_source
+sleep(2)
+wirte_youku_file(read_id(youku_search("F:\youku.txt",1)),driver.page_source)
+#print driver.page_source
+driver.close()
+
+
+
