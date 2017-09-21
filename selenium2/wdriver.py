20db68754563febaecbe0118988ad19cf2f96bde
DirecTion1024 <fx18358196136@gmail.com>
2017-09-21 17:50:55 +0800

    add loop_content

--- selenium2/wdriver.py
+++ selenium2/wdriver.py
@@ -1,15 +1,17 @@
-import sys
+# -*- coding:UTF-8 -*-
+import os
 from selenium2 import webdriver 
+from time import sleep
 
-# driver = webdriver.Remote("http://127.0.0.1:8000/wd/hub",{"desiredCapabilities":
-#                                                                   {"chromeOptions":
-#                                                                    {"androidPackage":"com.iflytek.inputmethod",
-#                                                                     "androidUseRunningApp":True,
-#                                                                     "androidProcess":"com.iflytek.inputmethod.mmp",
-#                                                                     "androidDeviceSerial":"a5809d4"}
-#                                                                    }
-#                                                                   })
-driver = webdriver.Remote("http://127.0.0.1:8000/wd/hub",{"desiredCapabilities":
+command = 'adb shell am startservice -a com.iflytek.xiri2.START --es startmode text --es text 打开优酷'
+print command.decode("utf-8").encode("gbk")
+os.popen(command.decode("utf-8").encode("gbk"))
+sleep(2)
+content = "adb shell am start -n com.fiberhome.iptv/.FHIptv --es intentMsg " + "\"" + "http://itvmkt.ah163.net/youku/detail.html?video_id=3943" + "\""
+print content
+os.popen(content)
+sleep(2)
+driver = webdriver.Remote("http://127.0.0.1:8000/wd/hub", {"desiredCapabilities":
                                                           {"chromeOptions":
                                                            {"androidPackage":"com.fiberhome.iptv",
                                                             "androidUseRunningApp":True,
