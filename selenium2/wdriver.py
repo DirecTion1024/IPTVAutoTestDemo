57e6766824b49f729e27e2a3152bfc4484da0e5a
DirecTion1024 <fx18358196136@gmail.com>
2017-09-22 09:46:12 +0800

    adjust youku_h5_test

--- selenium2/wdriver.py
+++ selenium2/wdriver.py
@@ -36,7 +36,6 @@ def wirte_youku_file(urlid,content):
         
 def open_youku():
     command = 'adb shell am startservice -a com.iflytek.xiri2.START --es startmode text --es text 打开优酷'
-    print command.decode("utf-8").encode("gbk")
     os.popen(command.decode("utf-8").encode("gbk"))
 
 def open_chrome_driver():
@@ -50,13 +49,13 @@ def line_number(filelocation):
     row_len = len(fobj.readlines())
     return row_len
 
-looptime = line_number("f:\youku.txt")+1
+looptime = line_number("f:\youku.txt")
 print looptime
 open_youku()
 print("open youku SUCCESS")
 sleep(2)
-for i in range(1,looptime):
-    print(line_number("f:\youku.txt")+1)
+for i in range(1,looptime+1):
+    print("开始第" +str(i) +"次爬取")
     open_chrome_driver()
     print("start chrome_driver SUCCESS")
     sleep(1)
@@ -67,7 +66,7 @@ for i in range(1,looptime):
 #content = "adb shell am start -n com.fiberhome.iptv/.FHIptv --es intentMsg " + "\"" + "http://itvmkt.ah163.net/youku/detail.html?video_id=3943" + "\""
 #print content
 #os.popen(content)
-    print("openurl:"+youku_search("F:\youku.txt",1))
+    print("openurl:"+youku_search("F:\youku.txt",i))
     sleep(2)
     driver = webdriver.Remote("http://127.0.0.1:8000/wd/hub", {"desiredCapabilities":
                                                           {"chromeOptions":
@@ -79,16 +78,19 @@ for i in range(1,looptime):
                                                            }
                                                           }
                           )
+    print("get driver SUCCESS")
     driver.switch_to_frame("mainWin")
 #print driver.page_source
     print("swtich frame SUCCESS")
     sleep(2)
-    wirte_youku_file(read_id(youku_search("F:\youku.txt",1)),driver.page_source)
+    wirte_youku_file(read_id(youku_search("F:\youku.txt",i)),driver.page_source)
     print("wirte file SUCCESS")
     print("file content is:"+driver.page_source)
     shutdown_chrome_driver()
     print("shutdown chrome_driver SUCCESS")
-
+    print("第" +str(i) +"次爬取结束")
+print("全部爬取结束")
+print("共爬取页面："+str(looptime))
 
 
 
