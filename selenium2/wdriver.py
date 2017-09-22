734d4d70fb9c9ba0d4dfed226e518384ee485c69
DirecTion1024 <fx18358196136@gmail.com>
2017-09-22 08:33:28 +0800

    complete youku_h5_test

--- selenium2/wdriver.py
+++ selenium2/wdriver.py
@@ -40,32 +40,34 @@ def open_youku():
     os.popen(command.decode("utf-8").encode("gbk"))
 
 def open_chrome_driver():
-    subprocess.Popen("chromedriver.exe --url-base=wd/hub --port=8000 --adb-port=5037")
-    print("start chrome_driver SUCCESS")
+    subprocess.Popen("chromedriver.exe --url-base=wd/hub --port=8000 --adb-port=5037")    
 
 def shutdown_chrome_driver():
-    os.popen("TASKKILL /F /IM chromedriver.exe /T")
-    print("shutdown chrome_driver SUCCESS")   
+    os.popen("TASKKILL /F /IM chromedriver.exe /T")       
 
 def line_number(self,filelocation):
     fobj = open(filelocation,'r')        
     row_len = len(fobj.readlines())
     return row_len
 
-open_chrome_driver()
-sleep(1)
-open_youku()
-sleep(2)
-open_url(youku_search("F:\youku.txt",1))
+for i in range(1,line_number("f:\youku.txt")+1):
+    print(line_number("f:\youku.txt")+1)
+    open_chrome_driver()
+    print("start chrome_driver SUCCESS")
+    sleep(1)
 #command = 'adb shell am startservice -a com.iflytek.xiri2.START --es startmode text --es text 打开优酷'
 #print command.decode("utf-8").encode("gbk")
 #os.popen(command.decode("utf-8").encode("gbk"))
-
+    open_youku()
+    print("open youku SUCCESS")
+    sleep(2)
+    open_url(youku_search("F:\youku.txt",1))
 #content = "adb shell am start -n com.fiberhome.iptv/.FHIptv --es intentMsg " + "\"" + "http://itvmkt.ah163.net/youku/detail.html?video_id=3943" + "\""
 #print content
 #os.popen(content)
-sleep(2)
-driver = webdriver.Remote("http://127.0.0.1:8000/wd/hub", {"desiredCapabilities":
+    print("openurl:"+youku_search("F:\youku.txt",1))
+    sleep(2)
+    driver = webdriver.Remote("http://127.0.0.1:8000/wd/hub", {"desiredCapabilities":
                                                           {"chromeOptions":
                                                            {"androidPackage":"com.fiberhome.iptv",
                                                             "androidUseRunningApp":True,
@@ -75,12 +77,17 @@ driver = webdriver.Remote("http://127.0.0.1:8000/wd/hub", {"desiredCapabilities"
                                                            }
                                                           }
                           )
-
-driver.switch_to_frame("mainWin")
-sleep(2)
-wirte_youku_file(read_id(youku_search("F:\youku.txt",1)),driver.page_source)
+    driver.switch_to_frame("mainWin")
 #print driver.page_source
-shutdown_chrome_driver()
+    print("swtich frame SUCCESS")
+    sleep(2)
+    wirte_youku_file(read_id(youku_search("F:\youku.txt",1)),driver.page_source)
+    print("wirte file SUCCESS")
+    print("file content is:"+driver.page_source)
+    shutdown_chrome_driver()
+    print("shutdown chrome_driver SUCCESS")
+
+
 
 
 
